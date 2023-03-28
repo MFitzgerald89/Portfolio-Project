371db9473bdc49659e6a151e1811403f10d5cf7c
MFitzgerald89 <122069535+MFitzgerald89@users.noreply.github.com>
2023-03-27 22:18:19 -0400

    removes all exisitng prior code/ creates an partially populates index.html, styles.scss, app.js

---
+++ app.js
@@ -0,0 +1,19 @@
+const sections = document.querySelectorAll('.section');
+const sectBtns = document.querySelectorAll('.controls');
+const sectBtn = document.querySelectorAll('.control');
+const allSections = document.querySelector('.maint-content');
+
+
+function PageTransition() {
+  //Button click active class
+  for (let i = 0; i < sectBtn.length; i++){
+    sectBtn[i].addEventListener('click', () => {
+      let currentBtn = document.querySelectorAll('.active-btn');
+      currentBtn[0].classlist = currentBtn[0].className.replace('active-btn', '');
+      this.className += 'active-btn'
+    })
+  }
+}
+
+PageTransition: ;
+
